4ba04b7cc76cda980df7023a2a04c96a55aa99e6
bm3871 <bm3871@pleasantonusd.net>
2025-01-24 22:48:40 +0000

    first commit, selection sort code added

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,20 @@
 public class SelectionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        int n = input.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
     }
 }
