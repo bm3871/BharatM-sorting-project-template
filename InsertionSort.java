b6606e8fb92eb4796f0695e084ff9e186719f839
bm3871 <bm3871@pleasantonusd.net>
2025-01-24 22:58:02 +0000

    insertion sort method

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,19 @@
 public class InsertionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int n = input.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = input[i];
+            int j = i - 1;
+
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = key;
+        }
     }
 }
